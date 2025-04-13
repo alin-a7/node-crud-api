2f5d94331c3d6f0e49c06adb029786cfdda097fb
Alina <almaksimovic01@gmail.com>
2025-04-13 17:57:19 +0400

    Modify tests to use SQLite database

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,18 +1,37 @@
-import 'dotenv/config'
-import { DataSource } from 'typeorm'
+import dotenv from 'dotenv'
+import { DataSource, DataSourceOptions } from 'typeorm'
 
 import { User } from './users/model/UserEntity'
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT || '5432'),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: true,
-  logging: false,
-  entities: [User],
-  migrations: [],
-  subscribers: [],
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 })
+
+const isTestEnv = process.env.NODE_ENV === 'test'
+
+let dataSourceConfig = {} as DataSourceOptions
+
+if (isTestEnv) {
+  dataSourceConfig = {
+    type: 'sqlite',
+    database: ':memory:',
+    synchronize: true,
+    entities: [User],
+    logging: true,
+  }
+} else {
+  dataSourceConfig = {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT || '5432'),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: true,
+    entities: [User],
+    logging: true,
+  }
+}
+
+export const AppDataSource = new DataSource(dataSourceConfig)
+
